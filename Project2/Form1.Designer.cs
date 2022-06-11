475decc84226066f10638f151916d7a51e63b246
techinalrupali <rupa99birajdar@gmail.com>
2022-06-11 21:47:22 +0530

    Add project files.

---
+++ Project2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDeptId = new System.Windows.Forms.TextBox();
+            this.txtDeptName = new System.Windows.Forms.TextBox();
+            this.txtDeptLocation = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnCreateFolder = new System.Windows.Forms.Button();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.btnDirectoryInfo = new System.Windows.Forms.Button();
+            this.btnCreateDirectoryFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Dept ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dept Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dept Location";
+            // 
+            // txtDeptId
+            // 
+            this.txtDeptId.Location = new System.Drawing.Point(143, 73);
+            this.txtDeptId.Name = "txtDeptId";
+            this.txtDeptId.Size = new System.Drawing.Size(143, 20);
+            this.txtDeptId.TabIndex = 3;
+            // 
+            // txtDeptName
+            // 
+            this.txtDeptName.Location = new System.Drawing.Point(142, 127);
+            this.txtDeptName.Name = "txtDeptName";
+            this.txtDeptName.Size = new System.Drawing.Size(143, 20);
+            this.txtDeptName.TabIndex = 4;
+            // 
+            // txtDeptLocation
+            // 
+            this.txtDeptLocation.Location = new System.Drawing.Point(142, 187);
+            this.txtDeptLocation.Name = "txtDeptLocation";
+            this.txtDeptLocation.Size = new System.Drawing.Size(143, 20);
+            this.txtDeptLocation.TabIndex = 5;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(94, 271);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 41);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(220, 270);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 42);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateFolder
+            // 
+            this.btnCreateFolder.Location = new System.Drawing.Point(387, 94);
+            this.btnCreateFolder.Name = "btnCreateFolder";
+            this.btnCreateFolder.Size = new System.Drawing.Size(94, 31);
+            this.btnCreateFolder.TabIndex = 8;
+            this.btnCreateFolder.Text = "Create Folder";
+            this.btnCreateFolder.UseVisualStyleBackColor = true;
+            this.btnCreateFolder.Click += new System.EventHandler(this.btnCreateFolder_Click);
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Location = new System.Drawing.Point(387, 151);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(94, 31);
+            this.btnCreateFile.TabIndex = 9;
+            this.btnCreateFile.Text = "Create File";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // btnDirectoryInfo
+            // 
+            this.btnDirectoryInfo.Location = new System.Drawing.Point(387, 208);
+            this.btnDirectoryInfo.Name = "btnDirectoryInfo";
+            this.btnDirectoryInfo.Size = new System.Drawing.Size(116, 36);
+            this.btnDirectoryInfo.TabIndex = 10;
+            this.btnDirectoryInfo.Text = "Create DirectoryInfo Folder ";
+            this.btnDirectoryInfo.UseVisualStyleBackColor = true;
+            this.btnDirectoryInfo.Click += new System.EventHandler(this.btnDirectoryInfo_Click);
+            // 
+            // btnCreateDirectoryFile
+            // 
+            this.btnCreateDirectoryFile.Location = new System.Drawing.Point(387, 271);
+            this.btnCreateDirectoryFile.Name = "btnCreateDirectoryFile";
+            this.btnCreateDirectoryFile.Size = new System.Drawing.Size(116, 41);
+            this.btnCreateDirectoryFile.TabIndex = 11;
+            this.btnCreateDirectoryFile.Text = "Create Directoryinfo file";
+            this.btnCreateDirectoryFile.UseVisualStyleBackColor = true;
+            this.btnCreateDirectoryFile.Click += new System.EventHandler(this.btnCreateDirectoryFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 367);
+            this.Controls.Add(this.btnCreateDirectoryFile);
+            this.Controls.Add(this.btnDirectoryInfo);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnCreateFolder);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtDeptLocation);
+            this.Controls.Add(this.txtDeptName);
+            this.Controls.Add(this.txtDeptId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDeptId;
+        private System.Windows.Forms.TextBox txtDeptName;
+        private System.Windows.Forms.TextBox txtDeptLocation;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnCreateFolder;
+        private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.Button btnDirectoryInfo;
+        private System.Windows.Forms.Button btnCreateDirectoryFile;
+    }
+}
+
---
+++ Project2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateFolder_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\S&QBatch";
+            if(Directory.Exists(path))
+            {
+                MessageBox.Show("Folder is already exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+                MessageBox.Show("Folder is created");
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\S&QBatch\Test.txt";
+            if(File.Exists(path))
+            {
+                MessageBox.Show("File is already exists");
+            }
+            else
+            {
+                File.Create(path);
+                MessageBox.Show("File is created");
+            }
+        }
+        //DirectoryInfo class
+        private void btnDirectoryInfo_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\S&QBatch1";
+            DirectoryInfo di = new DirectoryInfo(path);
+            if(di.Exists)
+            {
+                MessageBox.Show("Folder is already exists");
+            }
+            else
+            {
+                di.Create();
+                MessageBox.Show("Folder is created");
+            }
+        }
+
+        private void btnCreateDirectoryFile_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\S&QBatch1\Test.txt";
+            FileInfo fi = new FileInfo(path);
+            if(fi.Exists)
+            {
+                MessageBox.Show("File is already exists");
+            }
+            else
+            {
+                fi.Create();
+                MessageBox.Show("file is created");
+            }
+        }
+    }
+}
